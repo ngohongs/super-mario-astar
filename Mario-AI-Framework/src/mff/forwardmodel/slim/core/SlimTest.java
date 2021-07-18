d184e02e7ad49b0fff7ee39d79579e5d9614ba84
David Šosvald <david.sosvald@email.cz>
2021-07-18 14:00:47 +0200

    Advance speed comparison using all framework level generators

--- Mario-AI-Framework/src/mff/forwardmodel/slim/core/SlimTest.java
+++ Mario-AI-Framework/src/mff/forwardmodel/slim/core/SlimTest.java
@@ -7,6 +7,7 @@ import engine.core.MarioTimer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class SlimTest {
     private static String getLevel(String filepath) {
@@ -19,8 +20,7 @@ public class SlimTest {
         }
         try {
             content = new String(Files.readAllBytes(Paths.get("." + filepath)));
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Level couldn't be loaded, please check the path provided with regards to your working directory.");
             System.exit(1);
         }
@@ -39,52 +39,49 @@ public class SlimTest {
         }
     }
 
+    private static final ArrayList<String> levelTypes = new ArrayList<>() {{
+        add("original");
+        add("krys");
+        add("ge");
+        add("hopper");
+        add("notch");
+        add("notchParam");
+        add("notchParamRand");
+        add("ore");
+        add("patternCount");
+        add("patternOccur");
+        add("patternWeightCount");
+    }};
+
     private static void advanceSpeedTest() {
-        for (int i = 1; i <= 35; i++) {
-            double originalTime = 0;
-            double slimTime = 0;
-            double slimWindowTime = 0;
-
-            String level;
-
-            if (i < 16) {
-                level = getLevel("./levels/original/lvl-" + i + ".txt");
-            } else if (i < 26) {
-                MarioLevelGenerator generator = new levelGenerators.notch.LevelGenerator();
-                try {
-                    level = generator.getGeneratedLevel(new MarioLevelModel(150, 16), new MarioTimer(5 * 60 * 60 * 1000));
-                } catch (IllegalArgumentException e) {
-                    System.out.println("Level generator failed.");
-                    continue;
-                }
-            } else {
-                MarioLevelGenerator generator = new levelGenerators.benWeber.LevelGenerator();
-                level = generator.getGeneratedLevel(new MarioLevelModel(150, 16), new MarioTimer(5 * 60 * 60 * 1000));
-            }
-
-            for (int j = 0; j < 10; j++) {
-                MarioGameSlim game = new MarioGameSlim(false, true);
-                game.runGame(new mff.agents.astar.Agent(), level, 200, 0, false);
-            }
-            for (int k = 0; k < 100; k++) {
-                MarioGameSlim game = new MarioGameSlim(false, true);
-                TestResult testResult = game.runGame(new mff.agents.astar.Agent(), level, 200, 0, false);
-                originalTime += testResult.originalTime;
-                slimTime += testResult.slimTime;
-                slimWindowTime += testResult.slimWindowTime;
-            }
-
-            if (i < 16)
-                System.out.println("orig - " + i + "," + originalTime + "," + slimTime + "," + slimWindowTime);
-            else if (i < 26)
-                System.out.println("notch - " + (i - 15) + "," + originalTime + "," + slimTime + "," + slimWindowTime);
+        for (String levelType : levelTypes) {
+            int levelCount;
+            if (levelType.equals("original"))
+                levelCount = 15;
             else
-                System.out.println("benWeber - " + (i - 25) + "," + originalTime + "," + slimTime + "," + slimWindowTime);
+                levelCount = 100;
 
-//          System.out.println("Level: " + i + " original update time: " + originalTime);
-//          System.out.println("Level: " + i + " slim update time: " + slimTime);
-//          System.out.println();
-//          System.out.print("Level " + i + ": ");
+            for (int i = 1; i <= levelCount; i++) {
+                double originalTime = 0;
+                double slimTime = 0;
+                double slimWindowTime = 0;
+
+                String level = getLevel("./levels/" + levelType + "/lvl-" + i + ".txt");
+
+//                for (int j = 0; j < 1; j++) {
+//                    MarioGameSlim game = new MarioGameSlim(false, true);
+//                    game.runGame(new mff.agents.astar.Agent(), level, 30, 0, false);
+//                }
+                for (int k = 0; k < 1; k++) {
+                    MarioGameSlim game = new MarioGameSlim(false, true);
+                    TestResult testResult = game.runGame(new mff.agents.astar.Agent(), level, 30, 0, false);
+                    originalTime += testResult.originalTime;
+                    slimTime += testResult.slimTime;
+                    slimWindowTime += testResult.slimWindowTime;
+                }
+
+                System.out.println(levelType + "-" + i + "," + originalTime + "," + slimTime + "," + slimWindowTime);
+            }
         }
     }
 }
